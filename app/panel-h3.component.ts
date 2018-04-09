39d55ddad7b12f80564fb785c3c6118734321566
pjs <peter@visibility.net>
2018-04-09 21:39:32 +0100

    Added pull-right to date

--- app/panel-h3.component.ts
+++ app/panel-h3.component.ts
@@ -43,7 +43,7 @@ angular.module('app').component('panelH3', {
     template: `
   <div class="col-md-1 alert alert-success centered space-right">
     <div class="badge date-time">{{vm.formatTime(vm.period.dt_txt)}}</div>
-    <div class="date-time">{{vm.formatDate(vm.period.dt_txt)}}</div>
+    <div class="date-time pull-right">{{vm.formatDate(vm.period.dt_txt)}}</div>
     <img ng-src="{{vm.getIconUrl(vm.period.weather[0].icon)}}">
     <div class="date-time">{{vm.period.weather[0].description}}</div>
     <div class="badge date-time">{{vm.formatTemp(vm.period.main.temp)}} &deg;C</div>
